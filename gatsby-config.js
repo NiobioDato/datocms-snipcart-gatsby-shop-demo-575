4045b0920aac4737798167d37e05f968844a0f8c
NiobioDato <50357520+NiobioDato@users.noreply.github.com>
2020-04-28 12:57:10 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,11 +26,6 @@ module.exports = {
       // Include GTM in development.
       // Defaults to false meaning GTM will only be loaded in production.
       includeInDevelopment: false,
-
-      // datalayer to be set before GTM is loaded
-      // should be an object or a function that is executed in the browser
-      // Defaults to null
-      defaultDataLayer: { platform: "gatsby" },
     },
   ],
 }
